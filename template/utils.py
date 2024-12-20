4d33e0d4fdfb684daa15dcf7181d76703f081541
Jan Molenaar <molenaarjana@gmail.com>
2024-12-20 08:48:47 +0100

    fixed format

--- template/utils.py
+++ template/utils.py
@@ -61,5 +61,5 @@ def findInGrid(grid, charToFind):
 			if char == charToFind:
 				return (y, x)
 
-	def distance(pos1, pos2):
-		return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+def distance(pos1, pos2):
+	return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
